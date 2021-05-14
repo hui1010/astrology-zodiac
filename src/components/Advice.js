b8e852d41895a15e426c24db0b82a6bac0d4c481
Huiyi Skårner <63238749+hui1010@users.noreply.github.com>
2021-05-14 22:04:32 +0200

    Added one more button for getting personal advice

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -2,14 +2,14 @@ import React, {useState, useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 
-import { random } from '../redux'
+import { random, personalCode } from '../redux'
 
 function Advice() {
 
     const year = useSelector(state => state.year.year)
     const month = useSelector(state => state.month.month)
     const day = useSelector(state => state.day.day)
-    const personalCode = year%100 + month + day
+    const personalId = year%100 + month + day
     const [personalAdvice, setPersonalAdvice] = useState('')
 
     const randomState = useSelector(state=>state.random.random)
@@ -17,11 +17,15 @@ function Advice() {
     const dispatch = useDispatch()
 
     const [randomId, setRandomId] = useState(1)
-    let adviceId = randomState? randomId :personalCode
+    let adviceId = randomState? randomId :personalId
     
-    const handleClick = () => {
-            dispatch(random())
-            setRandomId(Math.round(Math.random()*100)+1)
+    const handleRandomClick = () => {
+        dispatch(random())
+        setRandomId(Math.round(Math.random()*100)+1)
+    }
+
+    const handlePersonalClick = () => {
+        dispatch(personalCode())
     }
     
         
@@ -39,9 +43,10 @@ function Advice() {
     
     return (
         <div className="output-content-inner advice">
-            <p>You personal advice based on your birthday is</p>           
+            <p>You {randomState? `random` : `personal`} advice based on your birthday is</p>           
             <p className="result advice-result">{personalAdvice}</p>        
-            <button className="advice-button" onClick={handleClick}>Try a random one</button>
+            <button className="advice-button random" onClick={handleRandomClick}>Try a random one</button>
+            <button className="advice-button personal" onClick={handlePersonalClick}>Get my personal one</button>
             <p className="info">Advice from <a className="info-link" href="https://api.adviceslip.com/">Advice Slip JSON API</a></p>
         </div>
     )
